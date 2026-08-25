d695294c77e4ec1092848090868185feed452074
amarjeetmaan3 <amarjeetmaan33@gmail.com>
2026-08-25 20:43:56 +0530

    Add files via upload

---
+++ app/src/main/java/com/amarjeetmaan/ajlivestudio/ui/layout/LayoutViewModel.kt
@@ -0,0 +1,15 @@
+package com.amarjeetmaan.ajlivestudio.ui.layout
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class LayoutViewModel : ViewModel() {
+    var preset by mutableStateOf(LayoutPreset.FULL_CAMERA)
+        private set
+
+    fun setPreset(newPreset: LayoutPreset) {
+        preset = newPreset
+    }
+}
